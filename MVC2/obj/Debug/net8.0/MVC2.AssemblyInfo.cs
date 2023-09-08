680412a60da1923b4ee3bd3601fff084f2c9bb07
Mohab Ashraf <mohabmohab160@gmail.com>
2023-09-08 22:48:13 +0200

    change Theme

--- MVC2/obj/Debug/net8.0/MVC2.AssemblyInfo.cs
+++ MVC2/obj/Debug/net8.0/MVC2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46bd5350a0dcf6cbd05ea997b94cb2ad647ecb98")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
