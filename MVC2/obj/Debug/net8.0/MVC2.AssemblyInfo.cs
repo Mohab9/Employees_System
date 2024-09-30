75cade70102d6a992e4d52aa32813ddce4c7c127
Mohab Ashraf <mohabmohab160@gmail.com>
2024-09-30 21:46:33 +0300

    first commit

--- MVC2/obj/Debug/net8.0/MVC2.AssemblyInfo.cs
+++ MVC2/obj/Debug/net8.0/MVC2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+680412a60da1923b4ee3bd3601fff084f2c9bb07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c28a9bc91d94c97d9c167d54dee0bd8908c7a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
